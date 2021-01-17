90b3bb95604fd27fa6ccd29eb7bf2ab36ce7c7a8
sreenivasmanthri <sreenivas.manthri@trysapling.com>
2021-01-16 23:39:05 -0800

    added all files

---
+++ src/main/java/com/secrets/manager/springsecretsmanager/SpringTestRestController.java
@@ -0,0 +1,38 @@
+package com.secrets.manager.springsecretsmanager;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Mak Sophea
+ * @date : 1/3/2020
+ **/
+@RestController
+@RequestMapping(value = "/api/test")
+public class SpringTestRestController {
+
+    @Value(value = "${application.id}")
+    private String applicationId;
+
+    @Value(value = "${environment}")
+    private String environment;
+
+    @Value(value="${categories.types:#{null}}")
+    private String[] categoryTypes;
+
+    @RequestMapping(value = "v1", method = RequestMethod.GET)
+    public Map<String, Object> getProperties(HttpServletRequest request) {
+
+        final Map<String, Object> map = new HashMap<>();
+        map.put("applicationId", applicationId);
+        map.put("environment", environment);
+        map.put("types", categoryTypes);
+        return map;
+    }
+
+}
